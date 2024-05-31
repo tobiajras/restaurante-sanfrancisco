a26498df7e254455d8375ed873fecea212c2ad4c
tobyajras <tobyajras@gmail.com>
2024-05-31 00:50:02 -0300

    metadata og added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -9,9 +9,23 @@ const poppins = Poppins({
 });
 
 export const metadata = {
+  metadataBase: 'https://restaurante-sanfrancisco.vercel.app/',
   title: 'Restaurante San Francisco - Zompantitla 7, Coyoacán',
   description:
     'Con más de 20 años de experiencia, nos dedicamos con pasión a ofrecer experiencias culinarias únicas y a crear momentos inolvidables',
+  openGraph: {
+    title: 'Restaurante San Francisco - Zompantitla 7, Coyoacán',
+    description:
+      'Con más de 20 años de experiencia, nos dedicamos con pasión a ofrecer experiencias culinarias únicas y a crear momentos inolvidables',
+    url: 'https://restaurante-sanfrancisco.vercel.app/',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Restaurante San Francisco - Zompantitla 7, Coyoacán',
+    description:
+      'Con más de 20 años de experiencia, nos dedicamos con pasión a ofrecer experiencias culinarias únicas y a crear momentos inolvidables',
+  },
 };
 
 export default function RootLayout({ children }) {
